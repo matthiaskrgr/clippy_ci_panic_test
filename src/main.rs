d71aca02e5839626636f3a71aaff34a6a0fedbee
Matthias Krüger <matthias.krueger@famsik.de>
2023-07-25 20:22:08 +0200

    add the code that causes a panic

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// this simulates clippy panicking due to ICE
+// if we do not catch this in our CI; raise an error
+
+#![feature(rustc_attrs)]
+#[rustc_error(delay_span_bug_from_inside_query)]
+fn main() {}
