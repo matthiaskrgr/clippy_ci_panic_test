a6b500c09c6c967fb75fb86c904e46e31426cc4c
Matthias Krüger <matthias.krueger@famsik.de>
2023-08-04 18:19:11 +0200

    silence the lint that would prevent the ICE from firing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // this simulates clippy panicking due to ICE
 // if we do not catch this in our CI; raise an error
 
+#![allow(internal_features)]
 #![feature(rustc_attrs)]
 #[rustc_error(delay_span_bug_from_inside_query)]
 fn main() {}
