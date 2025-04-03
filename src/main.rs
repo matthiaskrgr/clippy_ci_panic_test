fbd27cd587625a085aff2233fd8a074183328593
Philipp Krones <hello@philkrones.com>
2025-04-03 22:07:38 +0200

    Replace rustc_error with Clippy internal trigger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 // this simulates clippy panicking due to ICE
 // if we do not catch this in our CI; raise an error
 
-#![allow(internal_features)]
-#![feature(rustc_attrs)]
-#[rustc_error(delayed_bug_from_inside_query)]
+pub fn it_looks_like_you_are_trying_to_kill_clippy() {}
 fn main() {}
